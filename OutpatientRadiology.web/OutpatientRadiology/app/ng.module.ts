438e6003ada59268e96392c0eabcd6d7842b0786
Scott Ferguson <esfergus@gmail.com>
2015-12-04 19:56:05 -0600

    Created routes, set up views.

---
+++ OutpatientRadiology.web/OutpatientRadiology/app/ng.module.ts
@@ -0,0 +1,9 @@
+﻿/// <reference path="../scripts/typings/angularjs/angular.d.ts" />
+module drApp {
+    'use strict';
+    
+    var angularApp = angular.module('drApp', [
+        'ui.bootstrap',
+        'ui.router'
+    ]);
+}
